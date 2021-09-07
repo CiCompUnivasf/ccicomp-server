24b15251bb20211b8dc6cdf1b0b54c7d3af4927f
iJhefe <jeffytersaraiva@gmail.com>
2021-09-07 13:56:36 -0300

    test(crypto)

--- libs/crypto/src/crypto.service.spec.ts
+++ libs/crypto/src/crypto.service.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BCRYPT_ROUNDS } from './constants';
 import { CryptoService } from './crypto.service';
 
 describe('CryptoService', () => {
   let service: CryptoService;
+  let module: TestingModule;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  const plain = 'ccicomp';
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
       providers: [CryptoService],
     }).compile();
 
     service = module.get<CryptoService>(CryptoService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterAll(() => module.close());
+
+  it('should bcrypt rounds cost be a valid and safe value', () => {
+    expect(BCRYPT_ROUNDS).toBeGreaterThanOrEqual(10);
+    expect(BCRYPT_ROUNDS).toBeLessThanOrEqual(12);
+  });
+
+  it('should "createHash" generate a hash with salt with valid length', async () => {
+    const spyGenSalt = jest.spyOn(service, 'generateSalt');
+
+    const { salt, hash } = await service.createHash(plain);
+
+    // a valid bcrypt size
+    // see more: https://github.com/kelektiv/node.bcrypt.js#hash-info
+    expect(hash).toHaveLength(60);
+    expect(salt).toHaveLength(29);
+
+    expect(spyGenSalt).toBeCalledTimes(1);
+  });
+
+  it('should "createHashSync" generate a hash with salt with valid length', () => {
+    const spyGenSaltSync = jest.spyOn(service, 'generateSaltSync');
+
+    const { salt, hash } = service.createHashSync(plain);
+
+    // a valid bcrypt size
+    // see more: https://github.com/kelektiv/node.bcrypt.js#hash-info
+    expect(hash).toHaveLength(60);
+    expect(salt).toHaveLength(29);
+
+    expect(spyGenSaltSync).toBeCalledTimes(1);
+  });
+
+  it('should created hash async and sync be true on compare', async () => {
+    const { hash } = await service.createHash(plain);
+    const { hash: hashSync } = service.createHashSync(plain);
+
+    const asyncHashIsValid = await service.compareHash({ plain, hash });
+
+    expect(asyncHashIsValid).toBeTruthy();
+
+    const syncHashIsValid = await service.compareHash({
+      plain,
+      hash: hashSync,
+    });
+
+    expect(syncHashIsValid).toBeTruthy();
   });
 });
