890470b32e57e498fadc4b3cd4658e0f339c44e1
iJhefe <jeffytersaraiva@gmail.com>
2021-09-07 17:56:02 -0300

    chore(user): adicionado constraints no DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -6,11 +6,13 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { IsUnique } from '@ccicomp/common/validator';
 import { Password, PasswordInstance } from '../entities/aggregates/password';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto implements Partial<User> {
   @Length(4, 50, { message: 'O login deve ter entre 4 e 50 caracteres' })
+  @IsUnique({ entity: User })
   login: string;
 
   @MinLength(8, { message: 'A senha deve conter pelo menos 8 caracteres' })
@@ -20,5 +22,6 @@ export class CreateUserDto implements Partial<User> {
   password: Password;
 
   @IsEmail({}, { message: 'Digite um email válido' })
+  @IsUnique({ entity: User })
   email: string;
 }
